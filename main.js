d6c93461600f8b3677cfc6bf8d3f4545e845a9da
tom <eaelton@gmail.com>
2023-11-26 10:35:21 -0300

    Adjust cube position in main.js

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ else
 
 			let s = 100 + i * 50;
 			let cube = new t.Mesh(new t.BoxGeometry(s, s, s), new t.MeshBasicMaterial({color: c , wireframe: true}));
-			cube.position.x = win.shape.x + (win.shape.w * .5);
+			cube.position.x = win.shape.x + (win.shape.w * .9);
 			cube.position.y = win.shape.y + (win.shape.h * .5);
 
 			world.add(cube);
